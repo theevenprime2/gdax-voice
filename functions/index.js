8ac5d404eeb072a1a8ee7104d511f012e6f66a95
Allen Chang <voxlol@outlook.com>
2017-07-08 14:00:18 -0500

    Latest commit

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,35 @@
+'use strict'; 
+
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 const Gdax = require('gdax');
 const GdaxClient = new Gdax.PublicClient(['LTC-USD']);
+const functions = require('firebase-functions');
 
-exports.getLTCPrice = (req, res) => {
+exports.getLTCPrice = functions.https.onRequest((req, res) => {
     const app = new ActionsSdkApp({request: req, response: res});
+    console.log("Request is: ", req);
+    console.log("App is: ", app);
 
     function mainIntent(app) {
+        console.log("Main intent called");
         let inputPrompt = app.buildInputPrompt(false, "Welcome. Say something");
-        app.ask(inputPrompt)
+        app.ask(inputPrompt);
     }
 
     function getPrice(app) {
-        return GdaxClient.getProductTicker((tick) => {
+        conosle.log("Get price called");
+        return GdaxClient.getProductTicker((err, tick) => {
             if(tick && tick.price) {
-                app.tell(false, "The current price of Litecoin is " + tick.price)
+                app.tell(false, "The current price of Litecoin is " + tick.price);
             } else {
-                app.tell(false, "Error retrieving Litecoin price")
+                app.tell(false, "Error retrieving Litecoin price");
             }
         })
     }
 
     // Map actions
     let actionMap = new Map();
-    actionMap.set(app.StandardIntents.MAIN, mainIntent)
-    actionMap.set(app.StandardIntents.TEXT, getPrice)
+    actionMap.set('main', mainIntent);
+    actionMap.set('get.price', getPrice);
     app.handleRequest(actionMap);
-}
+});
